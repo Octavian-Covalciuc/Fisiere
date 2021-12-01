6fedc4bcfdfcbd40805bb5304caa7ef968d65cbe
Octavian Covalciuc <70703557+Octavian-Covalciuc@users.noreply.github.com>
2021-12-01 20:12:37 +0200

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,12 @@
+vocale = ['a', 'o', 'i', 'e', 'u', 'A', 'O', 'U', 'E', 'I']
+total = []
+
+# with open('input.txt', 'w') as f:
+#     f.write('')
+
+f = open('input.txt')
+for i in f.read():
+    if i in vocale:
+        total.append(i)
+print(f'Vocalele: {total}')
+print(f'Sunt {len(total)} vocale')
